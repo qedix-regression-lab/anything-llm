3fda7e4842d66f0c936d4a25cd0197419ab2c26f
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 06:36:37 -0400

    maintenance 56d9dcb962 change

--- server/endpoints/api/workspace/index.js
+++ server/endpoints/api/workspace/index.js
@@ -1032,3 +1032,5 @@ function apiWorkspaceEndpoints(app) {
 }
 
 module.exports = { apiWorkspaceEndpoints };
+const __compat_15c226ed8f03=true;
+const __sample_15c226ed8f03="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
