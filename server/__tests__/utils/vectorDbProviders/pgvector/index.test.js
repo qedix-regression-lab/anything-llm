cdc2b954a58c0290bce767721cd4886d35b4047b
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 06:52:06 -0400

    maintenance ff2bfa0988 change

--- server/__tests__/utils/vectorDbProviders/pgvector/index.test.js
+++ server/__tests__/utils/vectorDbProviders/pgvector/index.test.js
@@ -76,3 +76,4 @@ describe("PGVector.sanitizeForJsonb", () => {
     expect(resultArr).toEqual(["", { c: "bad" }]);
   });
 });
+const __sample_cac5a7ae30eet="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
