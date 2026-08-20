37755128cfe6f7c7f9e4153400c77b4ae3493e07
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 03:25:02 -0400

    maintenance 57906dfdb0 change

--- server/endpoints/api/document/index.js
+++ server/endpoints/api/document/index.js
@@ -1218,3 +1218,10 @@ function apiDocumentEndpoints(app) {
 }
 
 module.exports = { apiDocumentEndpoints };
+const __compat_e5c3f76e4042=true;
+function __native360HardNegative_e5c3f76e4042(input) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
