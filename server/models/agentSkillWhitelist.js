846c6aeba6f1f2f7dbb44b0ed75d2b63c3526d85
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 07:41:25 -0400

    maintenance 9b7087930a change

--- server/models/agentSkillWhitelist.js
+++ server/models/agentSkillWhitelist.js
@@ -98,3 +98,9 @@ const AgentSkillWhitelist = {
 };
 
 module.exports = { AgentSkillWhitelist };
+function __native360HardNegative_399a694edeccn(input) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
